f58bc5500cbce1ca6b47c4b020a404fa74556251
AlexD <alex@getpantheon.com>
2020-12-15 17:25:59 +0200

    more whitespace

--- web/wp-config.php
+++ web/wp-config.php
@@ -75,6 +75,5 @@ if ( ! defined( 'WP_DEBUG' ) ) {
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
-
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
